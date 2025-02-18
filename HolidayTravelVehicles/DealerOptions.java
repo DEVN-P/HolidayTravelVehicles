2615b91e4976a6673c71bfe7c0d8b7b92fd75f2e
DEVINPL <107537174+DEVN-P@users.noreply.github.com>
2025-02-18 15:47:11 +0700

    Added methods to the DealerOptions Class

--- HolidayTravelVehicles/DealerOptions.java
+++ HolidayTravelVehicles/DealerOptions.java
@@ -8,4 +8,28 @@ public class DealerOptions {
         this.description = description;
         this.optionPrice = optionPrice;
     }
+
+    public String getOptionCode() {
+        return optionCode;
+    }
+
+    public void setOptionCode(String optionCode) {
+        this.optionCode = optionCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getOptionPrice() {
+        return optionPrice;
+    }
+
+    public void setOptionPrice(int optionPrice) {
+        this.optionPrice = optionPrice;
+    }
 }
